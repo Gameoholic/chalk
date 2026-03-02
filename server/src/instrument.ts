158da524a6523ebb7b59cb01b635f7fef31ada0a
Gameoholic <viner.atmic@gmail.com>
2026-03-02 17:59:14 +0200

    fix sentry import

--- server/src/instrument.ts
+++ server/src/instrument.ts
@@ -1,4 +1,4 @@
-import Sentry from "@sentry/node";
+import * as Sentry from "@sentry/node";
 
 Sentry.init({
     dsn: "https://c9f94b1866f2bb87870981e6d086ebd5@o4510975136432128.ingest.de.sentry.io/4510975149998160",
