69b03cd99933e203d059019834699ac67810c83a
Gameoholic <viner.atmic@gmail.com>
2026-03-07 11:53:31 +0200

    Remove debug message

--- client/src/main/App.tsx
+++ client/src/main/App.tsx
@@ -16,6 +16,5 @@ export default function App() {
         localStorage.setItem("theme", themeContext.theme);
     }, [themeContext.theme]);
 
-    console.log("Hello loaded from CI/CD pipeline!");
     return <CanvasLoader />;
 }
