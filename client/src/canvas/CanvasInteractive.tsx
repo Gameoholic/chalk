acaefafe8287da1beb32227e45a28532be237b2b
Gameoholic <viner.atmic@gmail.com>
2026-03-18 16:41:11 +0200

    feat: zoom in further

--- client/src/canvas/CanvasInteractive.tsx
+++ client/src/canvas/CanvasInteractive.tsx
@@ -446,7 +446,7 @@ function handleMouseEvents(
 
         const newZoom =
             e.deltaY < 0 ? camera.zoom * zoomFactor : camera.zoom / zoomFactor;
-        const clampedZoom = Math.max(0.01, Math.min(100, newZoom));
+        const clampedZoom = Math.max(0.01, Math.min(1000, newZoom));
 
         const worldX = camera.position.x + mouseX / camera.zoom;
         const worldY = camera.position.y + mouseY / camera.zoom;
